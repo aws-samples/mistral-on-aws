bad3355fb94e1e1535a650b0fb2b1882093cb394
ying-at-471112771538 <hoying@amazon.com>
2025-05-20 15:50:13 +0100

    Fixed Available Tools tab and updated README.md

--- MCP/MCP_Mistral_app_demo/src/server_configs.py
+++ MCP/MCP_Mistral_app_demo/src/server_configs.py
@@ -3,7 +3,7 @@ SERVER_CONFIGS = [
         StdioServerParameters(
             command="npx",
             args=["-y", "@modelcontextprotocol/server-google-maps"],
-            env={"GOOGLE_MAPS_API_KEY": "<ADD_GOOGLE_API_KEY>"}
+            env={"GOOGLE_MAPS_API_KEY": "<ADD_GOOGLE_MAP_API>"}
         ),
         StdioServerParameters(
             command="npx",
